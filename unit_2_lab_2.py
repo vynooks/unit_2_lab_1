65dc26e48166b7a130c1e1530ca0c4e87536dd9c
vynooks <brendalicerio2@gmail.com>
2026-02-01 16:04:17 -0700

    +header+inputs

---
+++ unit_2_lab_2.py
@@ -0,0 +1,16 @@
+'''
+programmer: Allyson Licerio
+date: January 30, 2026
+program: Unit_2_lab_1.py
+description: write a program that asks the user to enter an address and customer name as separate 
+elements.
+'''
+
+#user inputs shipping address and customer name, etc.
+civic_number=input("Enter Civic Number: ")
+street_name=input("Enter Street Name: ")
+city_name=input("Enter City Name: ")
+province_code=input("Enter Province Code: ")
+postal_code=input("Enter Postal Code: ")
+customer_name=input("Enter Customer Name: ")
+
